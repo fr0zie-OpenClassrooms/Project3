8202c1ff6f7c139bcf22692403f097e4aee19fbe
Axel Rayet <axel.rayet@outlook.com>
2020-10-09 13:38:10 +0200

    Adding project files

---
+++ README.md
@@ -0,0 +1,2 @@
+# OpenClassrooms
+My OpenClassrooms path repository
---
+++ project_3/main.py
@@ -0,0 +1,52 @@
+import random
+
+def gameLocation():
+    # Create an empty list to setup the game
+    grid = []
+    for row in range(15):
+        grid.append([])
+        for column in range(15):
+            grid[row].append([row, column])
+
+    return grid
+
+def wallsLocation():
+    """Method that sets default walls and randomizes middle walls location."""
+    grid = gameLocation()
+    # Create an empty list for walls location
+    defaultWalls = []
+
+    i = 0
+    for row in grid:
+        j = 0
+        for column in grid[i]:
+            if (grid[i][j] == [0, j] or grid[i][j] == [14, j] or grid[i][j] == [i, 0] or grid[i][j] == [i, 14]) and grid[i][j] != [0, 7] and grid[i][j] != [14, 7]:
+                defaultWalls.append([i, j])
+            j += 1
+        i += 1
+    
+    return defaultWalls
+
+def startAt():
+    grid = gameLocation()
+    return grid[0][7]
+
+def finishAt():
+    grid = gameLocation()
+    return grid[14][7]
+
+print('Walls:', wallsLocation())
+print('MacGyver:', startAt())
+print('Guardian:', finishAt())
+
+class Labyrinth:
+    pass
+
+class MacGyver:
+    pass
+
+class Guardian:
+    pass
+
+class GUI:
+    pass
