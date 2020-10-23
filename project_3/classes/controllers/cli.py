ca39d9ba0391cb4cbd4ad62e1bf9d19703a0276a
Axel Rayet <axel.rayet@outlook.com>
2020-10-23 12:59:55 +0200

    Removed duplicate directory

--- project_3/classes/controllers/cli.py
+++
@@ -1,7 +0,0 @@
-class CLIController:
-    def __init__(self, game):
-        self.game = game
-
-    def handle_control(self):
-        move = input()
-        return move
--- project_3/classes/models/player.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    """Class creating the player's character."""
-
-    def __init__(self, maze):
-        self.maze = maze
-        self.inventory = 0
-        self.x = self.maze.x
-        self.y = self.maze.y
